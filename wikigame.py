63498a483db1482fd08d495e7fe3074b87f37a58
DaveTanton <da.tanton@gmail.com>
2021-03-15 12:10:22 +0000

    Create wikigame.py

---
+++ wikigame.py
@@ -0,0 +1,40 @@
+import requests
+import random
+
+def searchFun(link):
+    se = requests.session()
+    re = se.get(link)
+    html = re.text
+    html = html.lower()
+    links = html.split("href=")
+    listoflinks = []
+    for l in links:
+        parts = l.split("\"")
+        if ("http" in parts[1]):
+            listoflinks .append(parts[1])
+    return listoflinks
+
+search = searchFun ("https://en.wikipedia.org/wiki/Special:Random")
+#search = searchFun("")
+comparelist=[]
+count = 0
+
+while True:
+
+    randomcall = random.randint(3, 9)
+    newsite = search[randomcall]
+
+    if not search:
+        print("Thats it! its all over. no more valid links")
+        print("I have followed ", count, "links")
+        print(comparelist)
+        break
+    elif newsite in comparelist:
+        print("I have followed ", count, "links before I repeated myself")
+        print ("last valid link was",newsite)
+        for link in comparelist:
+            print(link)
+        break
+    else:
+        comparelist.append(newsite)
+    count += 1
